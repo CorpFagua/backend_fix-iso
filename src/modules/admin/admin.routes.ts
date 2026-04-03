b3861cd17328fa4d608e78e76a0b1f5ad2abd1fa
CorpFagua <faguacorp21@gmail.com>
2026-04-02 21:06:59 -0500

    se implemento mejora en la gestion de roles y nuevo modulo de  gestion

--- src/modules/admin/admin.routes.ts
+++ src/modules/admin/admin.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { authMiddleware } from '../../middleware/auth';
+import { rbac } from '../../middleware/rbac';
 import * as ctrl from './admin.controller';
 
 const router = Router();
@@ -7,17 +8,27 @@ const router = Router();
 router.use(authMiddleware);
 
 // Roles
-router.get('/roles', ctrl.listRoles);
-router.post('/roles', ctrl.createRole);
-router.put('/roles/:id', ctrl.updateRole);
-router.get('/roles/:roleId/permissions', ctrl.getRolePermissions);
-router.put('/roles/:roleId/permissions', ctrl.updateRolePermissions);
+router.get('/roles', rbac('roles:read'), ctrl.listRoles);
+router.post('/roles', rbac('roles:create'), ctrl.createRole);
+router.put('/roles/:id', rbac('roles:update'), ctrl.updateRole);
+router.get('/roles/:roleId/permissions', rbac('roles:read'), ctrl.getRolePermissions);
+router.put('/roles/:roleId/permissions', rbac('roles:update'), ctrl.updateRolePermissions);
 
 // Permissions
-router.get('/permissions', ctrl.listPermissionsGrouped);
-router.get('/permissions/all', ctrl.listAllPermissions);
+router.get('/permissions', rbac('roles:read'), ctrl.listPermissionsGrouped);
+router.get('/permissions/all', rbac('roles:read'), ctrl.listAllPermissions);
+router.post('/permissions', rbac('permissions:manage'), ctrl.createPermission);
+router.put('/permissions/:id', rbac('permissions:manage'), ctrl.updatePermission);
+router.delete('/permissions/:id', rbac('permissions:manage'), ctrl.deletePermission);
 
 // Modules
 router.get('/modules', ctrl.listModules);
+router.get('/modules/with-permissions', ctrl.listModulesWithPermissions);
+router.post('/modules', rbac('modules:manage'), ctrl.createModule);
+router.put('/modules/:id', rbac('modules:manage'), ctrl.updateModule);
+router.delete('/modules/:id', rbac('modules:manage'), ctrl.deleteModule);
+
+// User effective permissions
+router.get('/users/:id/effective-permissions', rbac('users:read'), ctrl.getUserEffectivePermissions);
 
 export default router;
